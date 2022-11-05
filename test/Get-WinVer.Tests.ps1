69bfd7c5cdd51d79381c7cfb2bb78993fd83516e
Marius <m@mari.us.com>
2022-11-05 22:52:24 +0100

    mod tests for ci

--- test/Get-WinVer.Tests.ps1
+++ test/Get-WinVer.Tests.ps1
@@ -5,6 +5,11 @@ BeforeAll {
 		-Path $PSScriptRoot/../chrupd.ps1 
     #>
     . $PSScriptRoot\..\chrupd.ps1 -cAutoUp 0
+    if ($env:CI) {
+        $testVer = $(@{osFullName="`"Windows Server 2016`" (10.0, Server)"; tsModeName="Normal"; tsMode=1;} | ConvertTo-Json)
+    } else {
+        $testVer = $(@{osFullName="`"Windows 10`" (10.0, Workstation)"; tsModeName="Normal"; tsMode=1;} | ConvertTo-Json)
+    }
 }
 
 <# TODO: mocks
@@ -14,7 +19,6 @@ BeforeAll {
 
 Describe "Get-WinVer" {
     It "Get Windows Version and supported Task Scheduler Mode" {
-        Get-WinVer -osInfo $osObj -tsModeNum 1 | ConvertTo-Json | Should -Be `
-            $(@{osFullName="`"Windows 10`" (10.0, Workstation)"; tsModeName="Normal"; tsMode=1;} | ConvertTo-Json)
+        Get-WinVer -osInfo $osObj -tsModeNum 1 | ConvertTo-Json | Should -Be $testVer
     }
 }
